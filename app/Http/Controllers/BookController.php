56eb3294c01b739114aaac30240c8fd928a28bcd
POKA TCHONENG Hermann <pokatchoneng@yahoo.fr>
2020-01-18 11:24:31 +0100

    complete app

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use Illuminate\Http\Request;
+use App\Http\Resources\BookCollection;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::all()->toArray();
+        return array_reverse($books);
+    }
+
+    public function add(Request $request){
+        $book = new Book([
+            'name' => $request->input('name'),
+            'author' => $request->input('author')
+        ]);
+        $book->save();
+
+        return response()->json('The book successfully added');
+    }
+
+    public function edit($id){
+        $book = Book::find($id);
+        return response()->json($book);
+    }
+
+    public function update($id, Request $request){
+        $book = Book::find($id);
+        $book->update($request->all());
+
+        return response()->json('The book successfully updated');
+    }
+
+    public function delete($id){
+        $book = Book::find($id);
+        $book->delete();
+
+        return response()->json('The book successfully deleted');
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    /*public function edit(Book $book)
+    {
+        //
+    }*/
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    /*
+    public function update(Request $request, Book $book)
+    {
+        //
+    }*/
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book $book)
+    {
+        //
+    }
+}
